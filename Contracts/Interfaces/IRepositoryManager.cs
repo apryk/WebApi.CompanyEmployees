0dfe1c3ce67a4bf8415b529f3d1bd0bb55a58ad4
Aleksandr Prykhodko <aleksandr.prykhodko@akumyn.com.au>
2021-08-01 13:57:00 +1000

    employees using async

---
+++ Contracts/Interfaces/IRepositoryManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Contracts.Interfaces
+{
+    public interface IRepositoryManager
+    {
+        ICompanyRepository Company { get; }
+        IEmployeeRepository Employee { get; }
+        Task SaveAsync();
+    }
+}
